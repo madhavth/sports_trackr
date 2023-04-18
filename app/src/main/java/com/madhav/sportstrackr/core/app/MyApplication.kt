43aa20f0d54845fee42d2b90ff0ddf1f49aa8c9c
madhavth <madhavth77@gmail.com>
2023-04-18 16:30:19 -0600

    added firebase

--- app/src/main/java/com/madhav/sportstrackr/core/app/MyApplication.kt
+++ app/src/main/java/com/madhav/sportstrackr/core/app/MyApplication.kt
@@ -1,14 +1,21 @@
 package com.madhav.sportstrackr.core.app
 
 import android.app.Application
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+
 
 @HiltAndroidApp
 class MyApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true)
+            .build()
+        FirebaseFirestore.getInstance().firestoreSettings = settings
         Timber.plant(Timber.DebugTree())
     }
 }
